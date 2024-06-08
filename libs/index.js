e2be6c73d636ba8ae0b1de30a3e4b6ef93870271
ylxb23 <ylxb23@163.com>
2024-06-08 19:57:54 +0800

    add request token

--- libs/index.js
+++ libs/index.js
@@ -5,78 +5,95 @@
 // 加载轮播图
 $(function () {
     // 用jquery加载接口获取轮播图数据
-    $.get("http://ncyunhua.com:8088/banner", function (data) {
-        console.log("获取到Banner接口数据: " + JSON.stringify(data));
-        var bannerHtml = "";
+    $.ajax({
+        url: "http://ncyunhua.com:8088/banner", 
+        type: "GET",
+        headers: {
+            "Authorization": localStorage.getItem("authorization"),
+        },
+        success: function (data) {
+            console.log("获取到Banner接口数据: " + JSON.stringify(data));
+            var bannerHtml = "";
 
-        var items = [];
-        if(data.code == 200) {
-            // 接口请求成功
-            items = data.data;
-        } else {
-            alert("banner接口请求失败！将加载默认配置。");
-            // 使用默认值
-            items = [
-                {
-                    "ID": 1,
-                    "Title": "广东深圳xxx大厦正式动工",
-                    "Image": "http://ncyunhua.com:8088/file/banner/1.jpg"
-                },
-                {
-                    "ID": 2,
-                    "Title": "江西南昌xxx地铁正式动工",
-                    "Image": "http://ncyunhua.com:8088/file/banner/2.jpg"
-                },
-                {
-                    "ID": 3,
-                    "Title": "北京xx广场xx大厦宣布倒闭",
-                    "Image": "http://ncyunhua.com:8088/file/banner/3.jpg"
-                },
-                {
-                    "ID": 4,
-                    "Title": "上海陆家嘴xx电脑商城宣布倒闭",
-                    "Image": "http://ncyunhua.com:8088/file/banner/4.jpg"
-                }
-            ];
+            var items = [];
+            if(data.code == 200) {
+                // 接口请求成功
+                items = data.data;
+            } else {
+                alert("banner接口请求失败！将加载默认配置。");
+                // 使用默认值
+                items = [
+                    {
+                        "ID": 1,
+                        "Title": "广东深圳xxx大厦正式动工",
+                        "Image": "http://ncyunhua.com:8088/file/banner/1.jpg"
+                    },
+                    {
+                        "ID": 2,
+                        "Title": "江西南昌xxx地铁正式动工",
+                        "Image": "http://ncyunhua.com:8088/file/banner/2.jpg"
+                    },
+                    {
+                        "ID": 3,
+                        "Title": "北京xx广场xx大厦宣布倒闭",
+                        "Image": "http://ncyunhua.com:8088/file/banner/3.jpg"
+                    },
+                    {
+                        "ID": 4,
+                        "Title": "上海陆家嘴xx电脑商城宣布倒闭",
+                        "Image": "http://ncyunhua.com:8088/file/banner/4.jpg"
+                    }
+                ];
+            }
+            for (const item of items) {
+                bannerHtml = bannerHtml + 
+                            `<div class="item">
+                                <img src="${item.Image}" alt="">
+                                <ul>
+                                    <li class="roll-title">${item.Title}</li>
+                                </ul>
+                            </div>`;
+            }
+            $("#carousel-banner").html(bannerHtml);
+        },
+        error: function (xhr, status, error) {
+            // alert("轮播图获取失败~, 状态: " + status)
         }
-        for (const item of items) {
-            bannerHtml = bannerHtml + 
-                        `<div class="item">
-                            <img src="${item.Image}" alt="">
-                            <ul>
-                                <li class="roll-title">${item.Title}</li>
-                            </ul>
-                        </div>`;
-        }
-        $("#carousel-banner").html(bannerHtml);
     });
 });
 
 // 推荐好物接口：http://ncyunhua.com:8088/recommend
 // 加载推荐好物列表
 $(function () {
-    $.get("http://ncyunhua.com:8088/recommend", function (data) {
-        console.log("获取到推荐好物接口数据：" + JSON.stringify(data));
-        var recommendHtml = "";
-        var items = [];
-        if(data.code == 200) {
-            items = data.data;
-        } else {
-            alert("推荐好物接口加载失败！");
+    $.ajax({
+        url: "http://ncyunhua.com:8088/recommend", 
+        type: "GET",
+        headers: {
+            "Authorization": localStorage.getItem("authorization"),
+        },
+        success: function (data) {
+            console.log("获取到推荐好物接口数据：" + JSON.stringify(data));
+            var recommendHtml = "";
+            var items = [];
+            if(data.code == 200) {
+                items = data.data;
+            } else {
+                alert("推荐好物接口加载失败！");
+            }
+            for (var item of items) {
+                recommendHtml += ` <div class="shop-item">
+                                    <ul>
+                                        <li class="pic">
+                                            <a href="javascript:;">
+                                                <img src="${item.image}" alt="" />
+                                            </a>
+                                        </li>
+                                        <li class="name">${item.name}</li>
+                                    </ul>
+                                </div>`
+            }
+            $("#recommend-shops").html(recommendHtml);
         }
-        for (var item of items) {
-            recommendHtml += ` <div class="shop-item">
-                                <ul>
-                                    <li class="pic">
-                                        <a href="javascript:;">
-                                            <img src="${item.image}" alt="" />
-                                        </a>
-                                    </li>
-                                    <li class="name">${item.name}</li>
-                                </ul>
-                            </div>`
-        }
-        $("#recommend-shops").html(recommendHtml);
     });
 });
 
@@ -84,46 +101,53 @@ $(function () {
 // 新闻信息接口：http://ncyunhua.com:8088/getNews
 // 加载新闻信息
 $(function () {
-    $.get("http://ncyunhua.com:8088/getNews", function (data) {
-        console.log("获取到新闻信息接口数据：" + JSON.stringify(data));
-        
-        // 新闻区分为4个模块，分别置于4个div中：id="news-id1" id="news-id2" id="news-id3" id="news-id4"
-        var newsId1Html = "";
-        var newsId2Html = "";
-        var newsId3Html = "";
-        var newsId4Html = "";
-        
-        var newsId1 = {};
-        var newsId2 = {};
-        var newsId3 = {};
-        var newsId4 = {};
-        if(data.code == 200) {
-            var news = data.data;
-            for (const item of news) {
-                if(item.ID == 1) {
-                    newsId1 = item;
-                } else if(item.ID == 2) {
-                    newsId2 = item;
-                } else if(item.ID == 3) {
-                    newsId3 = item;
-                } else if(item.ID == 4) {
-                    newsId4 = item;
-                } else {
-                    console.log("新闻信息接口数据ID未知！")
+    $.ajax({
+        url: "http://ncyunhua.com:8088/getNews", 
+        type: "GET",
+        headers: {
+            "Authorization": localStorage.getItem("authorization"),
+        },
+        success: function (data) {
+            console.log("获取到新闻信息接口数据：" + JSON.stringify(data));
+            
+            // 新闻区分为4个模块，分别置于4个div中：id="news-id1" id="news-id2" id="news-id3" id="news-id4"
+            var newsId1Html = "";
+            var newsId2Html = "";
+            var newsId3Html = "";
+            var newsId4Html = "";
+            
+            var newsId1 = {};
+            var newsId2 = {};
+            var newsId3 = {};
+            var newsId4 = {};
+            if(data.code == 200) {
+                var news = data.data;
+                for (const item of news) {
+                    if(item.ID == 1) {
+                        newsId1 = item;
+                    } else if(item.ID == 2) {
+                        newsId2 = item;
+                    } else if(item.ID == 3) {
+                        newsId3 = item;
+                    } else if(item.ID == 4) {
+                        newsId4 = item;
+                    } else {
+                        console.log("新闻信息接口数据ID未知！")
+                    }
                 }
+            } else {
+                alert("新闻信息接口加载失败！");
             }
-        } else {
-            alert("新闻信息接口加载失败！");
+            // newsHtml 组装
+            newsId1Html = assemblyNews(newsId1);
+            newsId2Html = assemblyNews(newsId2);
+            newsId3Html = assemblyNews(newsId3);
+            newsId4Html = assemblyNews(newsId4);
+            $("#news-id1").html(newsId1Html);
+            $("#news-id2").html(newsId2Html);
+            $("#news-id3").html(newsId3Html);
+            $("#news-id4").html(newsId4Html);
         }
-        // newsHtml 组装
-        newsId1Html = assemblyNews(newsId1);
-        newsId2Html = assemblyNews(newsId2);
-        newsId3Html = assemblyNews(newsId3);
-        newsId4Html = assemblyNews(newsId4);
-        $("#news-id1").html(newsId1Html);
-        $("#news-id2").html(newsId2Html);
-        $("#news-id3").html(newsId3Html);
-        $("#news-id4").html(newsId4Html);
     });
 
     function assemblyNews(item) {
--- libs/main.js
+++ libs/main.js
@@ -1,79 +1,101 @@
 // 公共的js脚本，即所有页面都需要导入的
 
+const TOKEN = "eyJhbGciOiJIUzUxMiJ9.eyJsb2dpbl91c2VyX2tleSI6Ijg5NGNkYWEzLTAyYWItNGFjYS1iYTYyLWE5OWQ2YTczMzQzMiJ9.YeahmlEqBVYiciHlihoOeveFYoCtnocdIO1jAIMcaEvbM5ydichQbjXy4v_LiJzWTAyDgRQ35gq-DMB-Xx-8Iw";
+localStorage.setItem("authorization", "Bearer " + TOKEN);
+
+
 // logo接口地址：http://ncyunhua.com:8088/logo
 // 加载logo
 $(function () {
-    $.get("http://ncyunhua.com:8088/logo", function (data) {
-        console.log("获取到Logo接口数据: " + JSON.stringify(data));
-        var logoUri = "";
-        if(data.code == 200) {
-            logoUri = data.data.Image;
-        } else {
-            alert("Logo接口请求失败！将加载默认图片。");
-            logoUri = "imgs/head-icon.png";
+    $.ajax({
+        url: "http://ncyunhua.com:8088/logo", 
+        type: "GET",
+        redirect: "follow",
+        headers: {
+            "Authorization": localStorage.getItem("authorization"),
+        },
+        success: function (data) {
+            console.log("获取到Logo接口数据: " + JSON.stringify(data));
+            var logoUri = "";
+            if(data.code == 200) {
+                logoUri = data.data.Image;
+            } else {
+                alert("Logo接口请求失败！将加载默认图片。");
+                logoUri = "imgs/head-icon.png";
+            }
+            $("#logo").attr("src", logoUri);
         }
-        $("#logo").attr("src", logoUri);
     });
 });
 
 // 加载导航栏
 $(function () {
     // 用jquery加载接口获取导航栏数据
-    $.get("http://ncyunhua.com:8088/title", function (data) {
-        console.log("获取到Title接口数据: " + JSON.stringify(data));
-        var nagativesHtml = "";
-        // 获取当前路径的最后一级目录
-        var paths = window.location.pathname.split("/");
-        var currPath = decodeURIComponent(paths[paths.length - 1]);
-        var items = [];
-        if(data.code == 200) {
-            // 接口请求成功
-            items = data.data;
-        } else {
-            alert("导航栏接口请求失败！将加载默认配置。");
-            // 使用默认值
-            items = [
-                {
-                    "ID": 1,
-                    "Name": "首页",
-                    "Url": "index.html"
-                },
-                {
-                    "ID": 2,
-                    "Name": "建筑图集",
-                    "Url": "建筑图集.html"
-                },
-                {
-                    "ID": 3,
-                    "Name": "求职招聘",
-                    "Url": "求职招聘.html"
-                },
-                {
-                    "ID": 4,
-                    "Name": "环球资讯",
-                    "Url": "环球资讯.html"
-                },
-                {
-                    "ID": 5,
-                    "Name": "班组信息",
-                    "Url": "班组信息.html"
-                },
-                {
-                    "ID": 6,
-                    "Name": "联盟公司",
-                    "Url": "联盟公司.html"
-                }
-            ];
-        }
-        for (const item of items) {
-            var liClazz = "";
-            if(item.Url === currPath) {
-                liClazz = "active";
+    $.ajax({
+        url: "http://ncyunhua.com:8088/title",
+        type: "GET",
+        headers: {
+            "Authorization": localStorage.getItem("authorization"),
+        },
+        success: function(data) {
+            console.log("获取到Title接口数据: " + JSON.stringify(data));
+            var nagativesHtml = "";
+            // 获取当前路径的最后一级目录
+            var paths = window.location.pathname.split("/");
+            var currPath = decodeURIComponent(paths[paths.length - 1]);
+            var items = [];
+            if(data.code == 200) {
+                // 接口请求成功
+                items = data.data;
+            } else {
+                alert("导航栏接口请求失败！将加载默认配置。");
+                // 使用默认值
+                items = [
+                    {
+                        "ID": 1,
+                        "Name": "首页",
+                        "Url": "index.html"
+                    },
+                    {
+                        "ID": 2,
+                        "Name": "建筑图集",
+                        "Url": "建筑图集.html"
+                    },
+                    {
+                        "ID": 3,
+                        "Name": "求职招聘",
+                        "Url": "求职招聘.html"
+                    },
+                    {
+                        "ID": 4,
+                        "Name": "环球资讯",
+                        "Url": "环球资讯.html"
+                    },
+                    {
+                        "ID": 5,
+                        "Name": "班组信息",
+                        "Url": "班组信息.html"
+                    },
+                    {
+                        "ID": 6,
+                        "Name": "联盟公司",
+                        "Url": "联盟公司.html"
+                    }
+                ];
             }
-            nagativesHtml = nagativesHtml + `<li class="${liClazz}"><a href="./${item.Url}">${item.Name}</a></li>`;
+            for (const item of items) {
+                var liClazz = "";
+                if(item.Url === currPath) {
+                    liClazz = "active";
+                }
+                nagativesHtml = nagativesHtml + `<li class="${liClazz}"><a href="./${item.Url}">${item.Name}</a></li>`;
+            }
+            $("#nagatives").html(nagativesHtml);
         }
-        $("#nagatives").html(nagativesHtml);
     });
+    // $.get("http://ncyunhua.com:8088/title", function (data) {
+        
+    // });
 });
 
 // 加载 banner
